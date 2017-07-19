26e00c6f10f4993b8da5595c3d447ddc3cf21b6e
Steven Jens Jorgensen <stevenjensj@gmail.com>
2017-07-19 11:47:11 -0500

    added teleop code

--- val_logic_manager/setup.py
+++ val_logic_manager/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['button_gui'],
+    packages=['button_gui', 'teleop'],
     package_dir={'': 'src'}
 )
 
